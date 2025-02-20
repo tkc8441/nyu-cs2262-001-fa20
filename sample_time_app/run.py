8af4627440488d38a47bbd00f8f4f3c097b525dc
tkc8441 <tkc8441@nyu.edu>
2025-02-20 18:27:42 -0500

    Add new time route

--- sample_time_app/run.py
+++ sample_time_app/run.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from datetime import datetime
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
     return 'Hello world!'
+
+#New route for time
+@app.route('/time')
+def current_time():
+    return str(datetime.now().time())
 
 
 app.run(host='0.0.0.0',
